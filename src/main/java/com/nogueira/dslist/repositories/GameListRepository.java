b144aed554064c6e5f2da9edcb813ab9ef9dfc52
Nog <lucas.nogueiramartins@gmail.com>
2023-05-11 17:25:59 -0300

    Find all lists - Implement GameList

---
+++ src/main/java/com/nogueira/dslist/repositories/GameListRepository.java
@@ -0,0 +1,9 @@
+package com.nogueira.dslist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.nogueira.dslist.entities.GameList;
+
+public interface GameListRepository extends JpaRepository<GameList, Long>{
+
+}
