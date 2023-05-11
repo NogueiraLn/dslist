46d003b81f0d255939e6d2bc77f412c4a47f754f
Nog <lucas.nogueiramartins@gmail.com>
2023-05-11 02:24:09 -0300

    Get Game By Id

--- src/main/java/com/nogueira/dslist/services/GameService.java
+++ src/main/java/com/nogueira/dslist/services/GameService.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.nogueira.dslist.dto.GameDTO;
 import com.nogueira.dslist.dto.GameMinDTO;
 import com.nogueira.dslist.entities.Game;
 import com.nogueira.dslist.repositories.GameRepository;
+
 
 @Service
 public class GameService {
@@ -15,9 +18,16 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
 	}
 }
 	
